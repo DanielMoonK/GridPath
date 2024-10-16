ebfc605a4f5afaebc1f099517dd5de911b790625
DanielMoonK <danielk213@nycstudents.net>
2024-10-16 16:35:30 +0000

    first commit

---
+++ GridPath.java
@@ -0,0 +1,33 @@
+public class GridPath
+{
+    /** Initialized in the constructor with distinct values that never change */
+    private int[][] grid;
+
+    public GridPath(int[][] newGrid){
+        grid = mewGrid;
+    }
+
+    /**
+    * Returns the Location representing a neighbor of the grid element at row and col,
+    * as described in part (a)
+    * Preconditions: row is a valid row index and col is a valid column index in grid.
+    * row and col do not specify the element in the last row and last column of grid.
+    */
+    public Location getNextLoc(int row, int col)
+    {
+        if (row == 4 && col != 4){
+            return new Location(4, col + 1);
+        }
+    }
+
+    /**
+    * Computes and returns the sum of all values on a path through grid, as described in
+    * part (b)
+    * Preconditions: row is a valid row index and col is a valid column index in grid.
+    * row and col do not specify the element in the last row and last column of grid.
+    */
+    public int sumPath(int row, int col)
+    {
+
+    }
+}
